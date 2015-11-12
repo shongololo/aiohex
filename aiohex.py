6b950fd2faf809176fcb8fa5b3c156f536123d46
shongololo <garethsimons@me.com>
2015-11-12 14:38:22 +0000

    no recursive breadth

--- aiohex.py
+++ aiohex.py
@@ -47,7 +47,6 @@ class AIOHex:
                         query_start = '[{0}|{1}'.format(link, key_relation)
                         query_end = query_start + '|\xff'
                         new_links = await redis_conn.execute('ZRANGEBYLEX', self.key_graph, query_start, query_end)
-                        print(inner_depth)
                         # extract keys
                         new_triggers = set([link.split('|')[2] for link in new_links])
                         # filter out links that have already been visited to prevent redundant links
@@ -60,6 +59,32 @@ class AIOHex:
             await inner_trace(search_depth, [start_id])
             # return once stack unwinds:
             return visited
+
+    async def traverse_breadth(self, search_depth, keys_start, key_relation):
+        keys_return = []
+        async with self.aioredis_conn_pool.get() as redis_conn:
+            if not isinstance(keys_start, list):
+                keys_start = list(keys_start)
+            keys_set = set(keys_start)
+            # convert start keys to tuple and store iteration depth
+            keys_start = [(0, key) for key in keys_start]
+            while True:
+                if not keys_start:
+                    break
+                breadth, id = keys_start.pop()
+                query_start = '[{0}|{1}'.format(id, key_relation)
+                query_end = query_start + '|\xff'
+                keys_new = await redis_conn.execute('ZRANGEBYLEX', self.key_graph, query_start, query_end)
+                # extract new keys
+                keys_new = [item.split('|')[2] for item in keys_new]
+                # check that keys haven't already been found
+                # don't cast new keys to set so that order is retained
+                keys_new = [key for key in keys_new if key not in keys_set]
+                keys_set.update(keys_new)
+                keys_return += keys_new
+                keys_start += [(breadth + 1, key) for key in keys_new if breadth < search_depth]
+        return keys_return
+
 
     # map some graph analysis functions to networkx?
 
