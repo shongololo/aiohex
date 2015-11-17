7e7f7b8edcd9016d2d736292e2439183be0bdf8f
Gareth Simons <garethsimons@me.com>
2015-11-17 09:41:48 +0000

    remove old recursive function

--- aiohex.py
+++ aiohex.py
@@ -33,33 +33,6 @@ class AIOHex:
             return await redis_conn.execute('ZRANGEBYLEX', self.key_graph, query_start, query_end)
 
 
-    async def traverse(self, search_depth, start_id, key_relation):
-        async with self.aioredis_conn_pool.get() as redis_conn:
-            # container for found links
-            visited = set()
-            # recursive function def
-            async def inner_trace(inner_depth, trigger_links):
-                if inner_depth == 0:
-                    return
-                else:
-                    inner_depth -= 1
-                    for link in trigger_links:
-                        query_start = '[{0}|{1}'.format(link, key_relation)
-                        query_end = query_start + '|\xff'
-                        new_links = await redis_conn.execute('ZRANGEBYLEX', self.key_graph, query_start, query_end)
-                        # extract keys
-                        new_triggers = set([link.split('|')[2] for link in new_links])
-                        # filter out links that have already been visited to prevent redundant links
-                        filtered_triggers = new_triggers.difference(visited)
-                        # update visited set
-                        visited.update(filtered_triggers)
-                        # send to next recursion
-                        await inner_trace(inner_depth, filtered_triggers)
-            # call recursive func
-            await inner_trace(search_depth, [start_id])
-            # return once stack unwinds:
-            return visited
-
     async def traverse_breadth(self, search_depth, keys_start, key_relation):
         keys_return = []
         async with self.aioredis_conn_pool.get() as redis_conn:
